1e04aa8cbc72a755ee00973103007a9176702f5c
robert3 <robert3@uni-bremen.de>
2021-01-04 12:22:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/digital_detox_displayontime/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.digital_detox_displayontime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Version 1.0 (04-01-2021)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
