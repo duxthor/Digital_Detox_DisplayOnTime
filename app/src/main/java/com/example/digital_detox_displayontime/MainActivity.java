50fd56c34f846fd6d5822ff2696772bb248c668c
robert3 <robert3@uni-bremen.de>
2021-01-04 12:24:07 +0100

    Added Names

--- app/src/main/java/com/example/digital_detox_displayontime/MainActivity.java
+++ app/src/main/java/com/example/digital_detox_displayontime/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //Version 1.0 (04-01-2021)
+    //Robert Lange and Daniela Scheling
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
