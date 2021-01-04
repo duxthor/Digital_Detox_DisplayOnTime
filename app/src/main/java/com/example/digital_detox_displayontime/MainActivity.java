af915904968e7051336d651bafefc66e2ea2aad1
robert3 <robert3@uni-bremen.de>
2021-01-04 17:24:58 +0100

    Added Timer

--- app/src/main/java/com/example/digital_detox_displayontime/MainActivity.java
+++ app/src/main/java/com/example/digital_detox_displayontime/MainActivity.java
@@ -1,16 +1,66 @@
 package com.example.digital_detox_displayontime;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static java.lang.Math.toIntExact;
+
+//Version 1.0 (04-01-2021)
+//Robert Lange and Daniela Scheling
 
 public class MainActivity extends AppCompatActivity {
 
-    //Version 1.0 (04-01-2021)
-    //Robert Lange and Daniela Scheling
+    private Chronometer chronometer;
+    private long pauseOffset;
+    private boolean running;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chronometer = findViewById(R.id.chronometer);
+        chronometer.setFormat("Time: %s");
+        chronometer.setBase(SystemClock.elapsedRealtime());
+
+        //Wird jede Sekunde ausgeführt
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if (SystemClock.elapsedRealtime() - chronometer.getBase() >= 10000) {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    Toast.makeText(MainActivity.this, "10 sec. over", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void startChronometer(View v) {
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+    public void pauseChronometer(View v) {
+        if (running) {
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+    public void resetChronometer(View v) {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
     }
 }
