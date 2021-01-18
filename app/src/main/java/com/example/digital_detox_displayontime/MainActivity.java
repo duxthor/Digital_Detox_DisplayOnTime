faee57113aecea11990f0d008c487c9400744b57
robert3 <robert3@uni-bremen.de>
2021-01-18 18:24:25 +0100

    Timer an wenn start und display an

--- app/src/main/java/com/example/digital_detox_displayontime/MainActivity.java
+++ app/src/main/java/com/example/digital_detox_displayontime/MainActivity.java
@@ -8,16 +8,22 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.hardware.display.DisplayManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.os.SystemClock;
 import android.provider.Settings;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -30,13 +36,8 @@ public class MainActivity extends AppCompatActivity {
     private Chronometer chronometer;
     private long pauseOffset;
     private boolean running;
-    private boolean isScreenOn;
-    private EditText editTextInput;
-
-    private SharedPreferences sharedPreferences;
-    private SharedPreferences.Editor editor;
-    public static String SCREENONTIME_COUNTER ="Screen on time Counter";
-    private TextView screenOnTime_view;
+    public boolean stopServiceClicked = false;
+    public boolean isScreenOn = false;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -44,98 +45,89 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextInput = findViewById(R.id.edit_text_input);
         chronometer = findViewById(R.id.chronometer);
 //      chronometer.setFormat("Time: %s");
         chronometer.setBase(SystemClock.elapsedRealtime());
 
-        sharedPreferences = getSharedPreferences("@strings/app_name", MODE_PRIVATE);
-        if (!checkUsageStatsAllowedOrNot()) {
-            Intent usageAccessIntent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
-            usageAccessIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(usageAccessIntent);
-            if (checkUsageStatsAllowedOrNot()) {
-                startService(new Intent(MainActivity.this, BackgroundService.class));
-            } else {
-                Toast.makeText(getApplicationContext(), "Berechtigungen werden benötigt.", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            startService(new Intent(MainActivity.this, BackgroundService.class));
-        }
-        screenOnTime_view = findViewById(R.id.screen_on_time);
-        TimerTask updateView = new TimerTask() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        long screen_on_time = sharedPreferences.getLong(SCREENONTIME_COUNTER,0);
-                        long second = (screen_on_time/1000)%60;
-                        long minute = (screen_on_time/(1000*60))%60;
-                        long hour = (screen_on_time/(1000*60*60));
-                        String screenOnTime_value = hour + " h " + minute + " m " + second +  " s ";
-                        screenOnTime_view.setText(screenOnTime_value);
-                    }
-                });
-            }
-        };
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(updateView,0, 1000);
+        // Start Service
+        Intent serviceIntent = new Intent(this, ForegroundService.class);
+        serviceIntent.putExtra("notification", "Die App läuft im Hintergrund.");
+        startService(serviceIntent);
 
-
-//        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
-//        new Timer().scheduleAtFixedRate(new TimerTask() {
+//        Timer timer = new Timer();
+//        timer.schedule(new TimerTask() {
 //            @Override
 //            public void run() {
-//                if (powerManager.isScreenOn() == true) {
-//                    Log.d(TAG, "isScreenOn: true");
-//                    if (!running) {
-//                        chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
-//                        chronometer.start();
-//                        running = true;
-//                    }
-//                } else if (powerManager.isScreenOn() == false) {
-//                    Log.d(TAG, "isScreenOn: false");
-//                    if (running) {
-//                        chronometer.stop();
-//                        pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
-//                        running = false;
-//                    }
-//                } else {
-//                    Log.d(TAG, "isScreenOn: Irgendetwas lief schief");
-//                }
+//
 //            }
-//        }, 0, 5000); //put here time 1000 milliseconds=1 second
+//        }, 0, 1000);
+//        timer.cancel();
 
 
-//        //Wird jede Sekunde ausgeführt
-//        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
-//            @Override
-//            public void onChronometerTick(Chronometer chronometer) {
-//                //Nach 10 Sekunden wird der Timer zurück gesetz
-//                if (SystemClock.elapsedRealtime() - chronometer.getBase() >= 10000) {
-//                    chronometer.setBase(SystemClock.elapsedRealtime());
-//                    Toast.makeText(MainActivity.this, "10 sec. over", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
+        //while (stopServiceClicked != true) {
+            // Prüft, ob Display an oder aus.
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH) {
+                DisplayManager displayManager = (DisplayManager) getSystemService(DISPLAY_SERVICE);
+                for (Display display : displayManager.getDisplays()) {
+                    if (display.getState() != Display.STATE_OFF) {
+                        if (isScreenOn != true) {
+                            Log.d(TAG, "DisplayManager_isScreenOn: true");
+                            isScreenOn = true;
+                            if (!running) {
+                                chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+                                chronometer.start();
+                                running = true;
+                            }
+                        }
+                    } else if (display.getState() == Display.STATE_OFF) {
+                        if (isScreenOn != false) {
+                            Log.d(TAG, "DisplayManager_isScreenOn: false");
+                            isScreenOn = false;
+                            if (running) {
+                                chronometer.stop();
+                                pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+                                running = false;
+                            }
+                        }
+                    }
+                }
+            } else {
+                PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+                if (powerManager.isScreenOn() == true) {
+                    if (isScreenOn != true) {
+                        Log.d(TAG, "PowerManager_isScreenOn: true");
+                        isScreenOn = true;
+                        if (!running) {
+                            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+                            chronometer.start();
+                            running = true;
+                        }
+                    }
+                } else if (powerManager.isScreenOn() == false) {
+                    if (isScreenOn != false) {
+                        Log.d(TAG, "PowerManager_isScreenOn: false");
+                        isScreenOn = false;
+                        if (running) {
+                            chronometer.stop();
+                            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+                            running = false;
+                        }
+                    }
+                } else {
+                    Log.d(TAG, "PowerManager: Irgendetwas lief schief");
+                }
+            }
+        }
+    //}
 
-    }
-
-    //Übermittelt einen String, der als Benachrichtigung angezeigt wird. Es verhindert
-    //das Beenden der App durch das System (ForegroundService)
-    public void startService(View v) {
-        String input = editTextInput.getText().toString();
-        Intent serviceIntent = new Intent(this, ForegroundService.class);
-        serviceIntent.putExtra("notification", input);
-        startService(serviceIntent);
-    }
 
     //Beendet den Service
     public void stopService(View v) {
         Intent serviceIntent = new Intent(this, ForegroundService.class);
         stopService(serviceIntent);
+        stopServiceClicked = true;
     }
+
 
     //Das onClick Attribut des Buttons versucht ein View zur Methode zu Schicken (vllt ohne onClick versuchen)
     public void startChronometer(View v) {
@@ -157,28 +149,5 @@ public class MainActivity extends AppCompatActivity {
     public void resetChronometer(View v) {
         chronometer.setBase(SystemClock.elapsedRealtime());
         pauseOffset = 0;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    public boolean checkUsageStatsAllowedOrNot() {
-        try{
-            PackageManager packageManager = getPackageManager();
-            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(getPackageName(), 0);
-            AppOpsManager appOpsManager = (AppOpsManager)getSystemService(APP_OPS_SERVICE);
-            int mode = appOpsManager.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, applicationInfo.uid, applicationInfo.packageName);
-            return (mode == AppOpsManager.MODE_ALLOWED);
-        } catch (Exception e) {
-            Toast.makeText(getApplicationContext(), "Fehler, es konnten keine Nutzungsdaten Manager gefunden werden.", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    @Override
-    protected void onDestroy() {
-        if (checkUsageStatsAllowedOrNot()) {
-            startService(new Intent(MainActivity.this, BackgroundService.class));
-        }
-        super.onDestroy();
     }
 }
